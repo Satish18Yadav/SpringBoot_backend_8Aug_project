94d6a844c494647f5b606b6047d887f923d88b3d
Satish <satishsyadav18@gmail.com>
2024-08-24 06:22:31 +0530

    using cascade type to define the relations between the entities through foreign keys

--- src/main/java/com/satish/shopsy/productservice8aug/model/Category.java
+++ src/main/java/com/satish/shopsy/productservice8aug/model/Category.java
@@ -1,11 +1,15 @@
 package com.satish.shopsy.productservice8aug.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -14,5 +18,7 @@ import lombok.Setter;
 public class Category extends BaseModel {
 
     private  String title;
-
+    @JsonIgnore
+    @OneToMany(mappedBy="category")
+    List<Product> products;
 }
--- src/main/java/com/satish/shopsy/productservice8aug/model/Product.java
+++ src/main/java/com/satish/shopsy/productservice8aug/model/Product.java
@@ -1,9 +1,9 @@
 package com.satish.shopsy.productservice8aug.model;
-
-
 import com.satish.shopsy.productservice8aug.dto.FakeStoreProductDTO;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -20,11 +20,10 @@ public class Product extends  BaseModel implements Serializable {
 
     private String title;
     private double price;
+    @ManyToOne(cascade={CascadeType.PERSIST})
     private Category category;
     private String Description;
 
     public String imageURL;
-
-
 
 }
